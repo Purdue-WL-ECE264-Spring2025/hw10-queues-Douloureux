c22387d5f74e8db3fde0b1c6d48a7220753caa96
Douloureux <Douloureux@users.noreply.github.com>
2025-04-04 12:11:28 -0400

    Implement queue functions

--- queue.c
+++ queue.c
@@ -1,8 +1,14 @@
 #include "queue.h"
 #include "tile_game.h"
 
-void enqueue(struct queue *q, struct game_state state) {}
+void enqueue(struct queue *q, struct game_state state) {
+  size_t value = serialize(state);
+  insert_at_head(&q->data, value);
+}
 
-struct game_state dequeue(struct queue *q) { return (struct game_state){0}; }
+struct game_state dequeue(struct queue *q) {
+  size_t value = remove_from_tail(&q->data);
+  return deserialize(value);
+}
 
 int number_of_moves(struct game_state start) { return 0; }
